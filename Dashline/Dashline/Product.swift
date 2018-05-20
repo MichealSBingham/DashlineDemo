437fc27e740bf02468199e6727a0ae3b6a3f26df
Micheal <micheal@swapapp.co>
2018-05-20 19:23:31 -0400

    merged my stuff

---
+++ Dashline/Dashline/Product.swift
@@ -0,0 +1,112 @@
+//
+//  Product.swift
+//  Dashline
+//
+//  Created by Micheal S. Bingham on 5/18/18.
+//  Copyright © 2018 Dashline Inc. All rights reserved.
+//
+
+
+import Foundation
+import RealmSwift
+
+
+
+class Product: Object{
+    
+    @objc dynamic var id = "0"
+    @objc dynamic var name = ""
+    @objc dynamic var price: Double = 0.0
+    @objc dynamic var quantity: Int = 0
+    @objc dynamic var tax_rate: Double = 0.0
+    @objc dynamic var total: Double = 0.0
+    @objc dynamic var total_tax: Double = 0.0
+    
+    
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+    
+    /// Obtains a Product Object by its ID - the barcode string. Returns a nil object if the object cannot be found. Checks if the object was added in shopping cart first, if not, pulls from database.
+    class func get(withID: String, then: (Product?) -> Void){
+        
+        
+        
+        if let item = uiRealm.object(ofType: Product.self, forPrimaryKey: withID){
+            // Item has been added to the shopping cart
+            
+            then(item)
+            
+        } else {
+            
+            // ******* Get From Database *******
+            then(nil)
+            // ******* Get From Database *******
+        }
+    }
+    
+   
+    
+    /// Adds a Product Object to the shopping cart. This function will update values of the 'Product'. Before modifying attributes of a Product object, call .beginWrite() and then call .commitWrite() afterwards. Example : try! uiRealm.beginWrite(); product!.quantity = quantity+1;  try! uiRealm.commitWrite()
+    func addToCart()  {
+        
+        
+        
+        try! uiRealm.write {
+            
+            uiRealm.add(self, update: true)
+            
+        }
+        
+        
+        
+    }
+    
+    
+    /// Calculates the total price of a particular product in the shopping cart based on its quantity, tax, and price. This function should be called whenever a product item is added or updated in the database.
+    func calculateTotal() {
+        print("Calculate Total")
+        var price = self.price * Double(self.quantity)
+        print("the price is: \(price)")
+        var tax = (tax_rate/100) * price
+        try! realm?.beginWrite()
+        self.total_tax = tax
+        self.total = price+tax
+        try! realm?.commitWrite()
+        self.addToCart()
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    // Call this whenever the quantity of a product is changed. Posts a notification that the quantity of a product has changed and passes the product as data in the notification under userInfo["product"].
+    func notifyQuantityChange()  {
+        NotificationCenter.default.post(name: .productQuantityDidChange, object: self, userInfo: ["product" : self])
+    }
+    
+    // Call this whenever a product is removed. Passes the product as data in the notification under userInfo["product"].
+    func notifyProductRemoved()  {
+        NotificationCenter.default.post(name: .productRemoved, object: self, userInfo: ["product": self])
+        
+    }
+    
+    // Call this whenever a product is added. Passes the product as data in the notification under userInfo["product"].
+    func notifyProductAdded()  {
+        NotificationCenter.default.post(name: .productAdded, object: self, userInfo: ["product": self])
+        
+    }
+    
+}
+
+extension Notification.Name {
+    
+    static let productQuantityDidChange = Notification.Name("productQuantityDidChange")
+    static let productRemoved = Notification.Name("productRemoved")
+    static let productAdded = Notification.Name("productAdded")
+}
+
---
+++ Dashline/Dashline/ShoppingCart.swift
@@ -0,0 +1,35 @@
+//
+//  ShoppingCart.swift
+//  Dashline
+//
+//  Created by Micheal S. Bingham on 5/20/18.
+//  Copyright © 2018 Dashline Inc. All rights reserved.
+//
+
+import Foundation
+
+
+class ShoppingCart{
+    
+    /// Returns the total tax of all items in the cart
+    class func getCartTaxTotal() -> Double{
+        var total = 0.0
+        let allProducts = uiRealm.objects(Product.self)
+        for product in allProducts{
+            total += product.total_tax
+        }
+        
+        return total
+    }
+    
+    /// Returns the total  of all items in the cart; including tax and discounts
+    class func getCartTotal() -> Double{
+        var total = 0.0
+        let allProducts = uiRealm.objects(Product.self)
+        for product in allProducts{
+            total += product.total
+        }
+        
+        return total
+    }
+}
