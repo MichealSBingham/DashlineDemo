2c44c12ce5bf966a707eff42ca16eef387b04677
David Slakter <davidslakter2@gmail.com>
2018-05-20 17:26:27 -0400

    Added Scroll View and Container View functionality

--- Dashline/Dashline/ContainerView.swift
+++ Dashline/Dashline/ContainerView.swift
@@ -12,12 +12,12 @@ import UIKit
 
 class ContainerView: UIViewController {
 
+    var scannerFrame: CGRect!
     var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
+        //add child views
         let homeVC = storyboard?.instantiateViewController(withIdentifier: "home")
         let scannerVC = storyboard?.instantiateViewController(withIdentifier: "scanner")
         let storeVC = storyboard?.instantiateViewController(withIdentifier: "chooseStore")
@@ -25,18 +25,46 @@ class ContainerView: UIViewController {
         self.addChildViewController(homeVC!)
         self.addChildViewController(scannerVC!)
         self.addChildViewController(storeVC!)
+        
+    
         scrollView = UIScrollView(frame: view.bounds)
-        let scrollWidth = view.frame.width
-        let scrollHeight = 3*view.frame.height
-        scrollView.contentSize = CGSize(width: scrollWidth, height: scrollHeight)
         
-        
+        //configure frames in scroll view
         scrollView.addSubview(homeVC!.view)
         scrollView.addSubview(scannerVC!.view)
         scrollView.addSubview(storeVC!.view)
         
+        var frame1 = homeVC!.view.frame
+        frame1.origin.y = frame1.height
+        homeVC!.view.frame = frame1
+        
+        scannerFrame = scannerVC!.view.frame
+        scannerFrame.origin.y = 2*scannerFrame.height
+        scannerVC!.view.frame = scannerFrame
+        
+        //configure scroll view
+        let scrollWidth = view.frame.width
+        let scrollHeight = 3*view.frame.height
+        scrollView.contentSize = CGSize(width: scrollWidth, height: scrollHeight)
+        scrollView.isPagingEnabled = true
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.isScrollEnabled = true
+        scrollView.setContentOffset(CGPoint(x: frame1.origin.x, y: frame1.origin.y+50), animated: false)
+        view.addSubview(scrollView)
+    
+    
+        //add notifications
+        NotificationCenter.default.addObserver(self, selector: #selector(self.showScanner), name: .showScanner, object: nil)
+        
+    }
+    
+    @objc func showScanner(){
+        scrollView.setContentOffset(CGPoint(x: scannerFrame.origin.x, y: scannerFrame.origin.y), animated: true)
         
     }
 
+}
 
+extension Notification.Name{
+    static let showScanner = Notification.Name("showScanner")
 }
---
+++ Dashline/Dashline/HomeView.swift
@@ -0,0 +1,28 @@
+//
+//  HomeView.swift
+//  Dashline
+//
+//  Created by David Slakter on 5/20/18.
+//  Copyright © 2018 Dashline Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+
+class HomeView: UIViewController{
+    @IBOutlet var scanButton: UIButton!
+    @IBOutlet var itemsList: UITextView!
+    
+   override func viewDidLoad() {
+        navigationController?.navigationBar.isHidden = true
+    }
+    @IBAction func showScanner(_ sender: Any) {
+        NotificationCenter.default.post(Notification(name: .showScanner))
+    }
+    @IBAction func showCheckout(_ sender: Any) {
+        performSegue(withIdentifier: "showCheckout", sender: nil)
+    }
+    
+}
